d2e037f27153063b87d24d5efa7479b7abaa323e
Bryan Yap <bryan.yap@alliants.com>
2014-05-28 09:16:41 +0100

    moved to sqlite3 for local dev

---
+++ app/assets/javascripts/serializers/iteration_serializer.js
@@ -0,0 +1,31 @@
+//= require ./serializer_helper
+
+Probe.IterationSerializer = DS.RESTSerializer.extend({
+  extract: function(store, type, payload, id, requestType) {
+    var newPayload = {};
+    console.log("Serializer: " + type + "." + requestType + "()");
+
+    if (requestType === "find") {
+      newPayload.iteration =
+        Probe.SerializerHelper.permit(payload, [
+          "kind", "project_id", "team_strength", "start", "finish", "stories",
+          "number"
+        ]);
+    } else {
+      var iterations = [];
+      for (var i = 0; i < payload.length; i++) {
+        iterations[iterations.length] = Probe.SerializerHelper.permit(payload[i], [
+          "kind", "project_id", "team_strength", "start", "finish", "stories",
+          "number"
+        ]);
+        // if (stories[stories.length - 1].labels.length == 0) {
+        //   delete stories[stories.length - 1].labels;
+        // }
+      }
+      newPayload.iterations = iterations;
+    }
+
+    // console.log(newPayload);
+    return this._super(store, type, newPayload, id, requestType);
+  }
+});
